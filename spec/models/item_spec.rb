60bad14545090e2d7ab98e99012ac28e78dc3ba4
kazu-1987 <otsuji.k1123@gmail.com>
2021-10-12 16:04:02 +0900

    修正箇所の修正完了

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -5,83 +5,87 @@ RSpec.describe Item, type: :model do
   end
 
   describe '商品出品' do
-    it 'item_nameが空では登録できない' do
-      @item.item_name = ''
-      @item.valid?
-      expect(@item.errors.full_messages).to include("Item name can't be blank")
+    context '内容に問題ない場合' do
+      it 'priceが300ちょうどだと登録できる' do
+        @item.price = '300'
+        @item.valid?
+        expect(@item.errors.full_messages)
+      end
+      it 'priceが9999999だと登録できる' do
+        @item.price = '9999999'
+        @item.valid?
+        expect(@item.errors.full_messages)
+      end
+      it "全てが入力されていれば保存できる" do
+        expect(@item).to be_valid
+      end
     end
-    it 'descriptionが空では登録できない' do
-      @item.description = ''
-      @item.valid?
-      expect(@item.errors.full_messages).to include("Description can't be blank")
-    end
-    it 'category_idが空では登録できない' do
-      @item.category_id = ''
-      @item.valid?
-      expect(@item.errors.full_messages).to include('Category Select')
-    end
-    it 'condition_idが空では登録できない' do
-      @item.condition_id = ''
-      @item.valid?
-      expect(@item.errors.full_messages).to include('Condition Select')
-    end
-    it 'delivery_fee_idが空では登録できない' do
-      @item.delivery_fee_id = ''
-      @item.valid?
-      expect(@item.errors.full_messages).to include('Delivery fee Select')
-    end
-    it 'prefecture_idが空では登録できない' do
-      @item.prefecture_id = ''
-      @item.valid?
-      expect(@item.errors.full_messages).to include('Prefecture Select')
-    end
-    it 'delivery_day_idが空では登録できない' do
-      @item.delivery_day_id = ''
-      @item.valid?
-      expect(@item.errors.full_messages).to include('Delivery day Select')
-    end
-    it 'priceが空では登録できない' do
-      @item.price = ''
-      @item.valid?
-      expect(@item.errors.full_messages).to include("Price can't be blank")
-    end
-    it 'priceが299以下だと登録できないこと' do
-      @item.price = '299'
-      @item.valid?
-      expect(@item.errors.full_messages).to include('Price Out of setting range')
-    end
-    it 'priceが300ちょうどだと登録できる' do
-      @item.price = '300'
-      @item.valid?
-      expect(@item.errors.full_messages)
-    end
-    it 'priceが9999999だと登録できる' do
-      @item.price = '9999999'
-      @item.valid?
-      expect(@item.errors.full_messages)
-    end
-    it 'priceが10000000だと登録できないこと' do
-      @item.price = '10000000'
-      @item.valid?
-      expect(@item.errors.full_messages).to include('Price Out of setting range')
-    end
-    it 'priceが全角だと登録できないこと' do
-      @item.price = '５００'
-      @item.valid?
-      expect(@item.errors.full_messages).to include('Price Out of setting range')
-    end
-    it 'userが紐付いていないと保存できない' do
-      @item.user = nil
-      @item.valid?
-      expect(@item.errors.full_messages).to include('User must exist')
-    end
-    it 'imageが空では保存できない'do
-      @item.image = nil
-      @item.valid?
-      expect(@item.errors.full_messages).to include("Image can't be blank")
-    end
-    it "全てが入力されていれば保存できる" do
-      expect(@item).to be_valid
+    context '内容に問題がある場合' do
+      it 'item_nameが空では登録できない' do
+        @item.item_name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item name can't be blank")
+      end
+      it 'descriptionが空では登録できない' do
+        @item.description = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Description can't be blank")
+      end
+      it 'category_idが空では登録できない' do
+        @item.category_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Category Select')
+      end
+      it 'condition_idが空では登録できない' do
+        @item.condition_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Condition Select')
+      end
+      it 'delivery_fee_idが空では登録できない' do
+        @item.delivery_fee_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Delivery fee Select')
+      end
+      it 'prefecture_idが空では登録できない' do
+        @item.prefecture_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Prefecture Select')
+      end
+      it 'delivery_day_idが空では登録できない' do
+        @item.delivery_day_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Delivery day Select')
+      end
+      it 'priceが空では登録できない' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it 'priceが299以下だと登録できないこと' do
+        @item.price = '299'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price Out of setting range')
+      end
+      it 'priceが10000000だと登録できないこと' do
+        @item.price = '10000000'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price Out of setting range')
+      end
+      it 'priceが全角だと登録できないこと' do
+        @item.price = '５００'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price Out of setting range')
+      end
+      it 'userが紐付いていないと保存できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include('User must exist')
+      end
+      it 'imageが空では保存できない'do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
     end
   end
 end
