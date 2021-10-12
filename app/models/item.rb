086d75d79a5cb6a2ef0a3b80f5bd32772df7db87
kazu-1987 <otsuji.k1123@gmail.com>
2021-10-12 17:44:50 +0900

    バリデーション見直し

--- app/models/item.rb
+++ app/models/item.rb
@@ -20,9 +20,9 @@ class Item < ApplicationRecord
   validates :price,
             numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'Out of setting range' }
   validates :price, numericality: { only_integer: true, message: 'Half-width number.' }
-  validates :condition_id, numericality: { other_than: 0, message: 'Select' }
-  validates :delivery_fee_id, numericality: { other_than: 0, message: 'Select' }
-  validates :prefecture_id, numericality: { other_than: 0, message: 'Select' }
-  validates :delivery_day_id, numericality: { other_than: 0, message: 'Select' }
-  validates :category_id, numericality: { other_than: 0, message: 'Select' }
+  validates :condition_id, numericality: { other_than: 1, message: 'Select' }
+  validates :delivery_fee_id, numericality: { other_than: 1, message: 'Select' }
+  validates :prefecture_id, numericality: { other_than: 1, message: 'Select' }
+  validates :delivery_day_id, numericality: { other_than: 1, message: 'Select' }
+  validates :category_id, numericality: { other_than: 1, message: 'Select' }
 end
