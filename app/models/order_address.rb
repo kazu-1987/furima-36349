8539977d7ef3fac024f648b214b4b1b54f64bca6
kazu-1987 <otsuji.k1123@gmail.com>
2021-10-23 01:02:06 +0900

    エラー表示実装

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,8 +1,7 @@
 class OrderAddress
+  include ActiveModel::Model
+  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building, :phone_number, :item_id, :user_id
 
-  include ActiveModel::Model​​
-  atter_accessor :postal_code, :prefecture_id, :city, :house_number, :building, :phone_number, :item_id, :user_id
-  
   with_options presence: true do
     validates :item_id
     validates :user_id
@@ -11,11 +10,10 @@ class OrderAddress
     validates :house_number
     validates :phone_number, format: {with: /\A[0-9]{11}\z/ }
   end
-  validates :order_id, numericality: { other_than: 1 }
+  validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
+
   def save
     order = Order.create(item_id: item_id, user_id: user_id)
     Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building: building, phone_number: phone_number, order_id: order.id)
   end
 end
-
-
